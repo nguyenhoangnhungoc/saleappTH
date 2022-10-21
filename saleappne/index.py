08ab3d6a586f42fc241a62315427f2f60b9094a3
nguyenhoangnhungoc <nguyenhoangnhungoc.1411@gmail.com>
2022-10-21 16:37:48 +0700

    update day 1

---
+++ saleappne/index.py
@@ -0,0 +1,18 @@
+from flask import render_template, request
+from saleappne import app
+from saleappne import dao
+
+
+@app.route('/')
+def index():
+    categories = dao.load_categories()
+
+    cate_id = request.args.get('category_id')
+
+    products = dao.load_products(cate_id)
+
+    return render_template('index.html', categories=categories, products=products)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
